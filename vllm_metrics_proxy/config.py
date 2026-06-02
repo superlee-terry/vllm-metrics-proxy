3bfab6825a2d0b8882e2e555c4dde7c275aa96c9
superlee-terry <lee21321@gmail.com>
2026-06-02 10:47:33 +0800

    chore: project scaffolding + config

---
+++ vllm_metrics_proxy/config.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    vllm_upstream: str = "http://localhost:8001"
+    proxy_port: int = 8000
+    db_path: str = "./metrics.db"
+    log_level: str = "INFO"
+
+    model_config = {"env_prefix": ""}
+
+
+settings = Settings()
